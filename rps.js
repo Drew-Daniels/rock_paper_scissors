d6d1aa4f942a12f9a558b82890f336321304c289
Drew Daniels <drew@thedaniels.com>
2021-07-25 22:43:11 -0500

    Created a 'playGame' function to run 5 rounds of Rock, Paper, Scissors and determine a winner. Also added in user input validation to make an educated guess if the user input is not valid.

--- rps.js
+++ rps.js
@@ -1,11 +1,36 @@
 let computerName = "The Computer"
 let playerName = "The Player"
 
+let computerScore = 0;
+let playerScore = 0;
+
 const options = ["rock", "paper", "scissors"]
 
-function computerPlay(arr) {
+function computerPlay(arr=options) {
     let play = arr[Math.floor(Math.random()*arr.length)];
     return play;
+}
+
+function validateEntry(entry, arr=options) {
+    if (!(arr.includes(entry))) {   // if not in list, make educated guess
+        let firstLetter = entry[0];
+        firstLetter = firstLetter.toLowerCase();
+        console.log(firstLetter);
+        if (firstLetter == "r") {
+            return "rock";
+        } else if (firstLetter == "p") {
+            return "paper";
+        } else if (firstLetter == "s") {
+            return "scissors";
+        } else {
+            return "rock";
+        }
+    }
+}
+
+function getInput(msg = "Rock, Paper, Scissors?") {
+    result = prompt(msg)
+    return result;
 }
 
 function prepareResults(winner, winnerPlay, loserPlay) {
@@ -21,9 +46,13 @@ function prepareResults(winner, winnerPlay, loserPlay) {
     if (winner === computerName) {
         winnerName = computerName; // computer wins
         loserName = playerName;
+        computerScore = ++computerScore;
+
     } else {
         winnerName = playerName; // player wins
-        loserName = computerName;        
+        loserName = computerName;
+        playerScore = ++playerScore;
+        
     }
 
     // Keep plays
@@ -56,7 +85,7 @@ function playRound(playerSelection, computerSelection) {
                 finMsg = prepareResults(playerName, playerSelection, computerSelection); // player win
             }
             break;
-        case (playerselection === "paper"):
+        case (playerSelection === "paper"):
             if (computerSelection === "scissors") {
                 finMsg = prepareResults(computerName, computerSelection, playerSelection);
             } else {
@@ -75,7 +104,34 @@ function playRound(playerSelection, computerSelection) {
     return finMsg
 }
 
-const playerSelection = "rock";
-//const computerSelection = computerPlay(options);
-const computerSelection = computerPlay(options);
-console.log(playRound(playerSelection, computerSelection));
+function playGame() {
+    let winner;
+    let loser;
+
+    let winScore;
+    let losScore;
+
+    console.log("Round 1: " + playRound(validateEntry(getInput()), computerPlay()));
+    console.log("Round 2: " + playRound(validateEntry(getInput()), computerPlay()));
+    console.log("Round 3: " + playRound(validateEntry(getInput()), computerPlay()));
+    console.log("Round 4: " + playRound(validateEntry(getInput()), computerPlay()));
+    console.log("Round 5: " + playRound(validateEntry(getInput()), computerPlay()));
+
+    if (computerScore === playerScore) {
+        return (`Tie: ${computerName} scored ${computerScore} and ${playerName} scored ${playerScore}`)
+    } else if (computerScore > playerScore) {
+        winner = computerName;
+        loser = playerName;
+        winScore = computerScore;
+        losScore = playerScore;
+    } else {
+        winner = playerName;
+        loser = computerName;
+        winScore = playerScore;
+        losScore = computerScore;
+    }
+
+    return (`${winner} won with a score of ${winScore}, vs. ${loser} with a score of ${losScore} `)
+}
+
+console.log(playGame());
